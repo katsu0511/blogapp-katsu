fd2bec951d3aa0d24e6e24376bf69b5daba81047
Katsuya <haradakatsuya190511@gmail.com>
2020-06-20 16:55:24 +0700

    titleとcontent合わせて100文字以上になるようにvalidationを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -22,6 +22,8 @@ class Article < ApplicationRecord
   validates :content, length: {minimum: 10}
   validates :content, uniqueness: true
 
+  validate :validate_title_and_content_length
+
   belongs_to :user
 
   def display_created_at
@@ -31,4 +33,11 @@ class Article < ApplicationRecord
   def author_name
     user.display_name
   end
+
+  private
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    errors.add(:content, '100文字以上で！') unless char_count > 100
+  end
+
 end
