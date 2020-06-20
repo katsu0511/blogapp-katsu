f72e967f3a9a7841f51e2206829f540551b402e4
Katsuya <haradakatsuya190511@gmail.com>
2020-06-20 16:24:28 +0700

    validationにuniquenessを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,8 +16,10 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: {minimum: 2, maximum: 100}
+
   validates :content, presence: true
   validates :content, length: {minimum: 10}
+  validates :content, uniqueness: true
 
   belongs_to :user
 
